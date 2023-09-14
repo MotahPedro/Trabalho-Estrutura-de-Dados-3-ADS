a05e08085f21215e5ae3036295afb23254924662
Leonardo Naves <112529255+Navesx0@users.noreply.github.com>
2023-09-14 18:40:29 -0300

    Integração com o front

--- Cadastrador.js
+++ Cadastrador.js
@@ -1,12 +1,12 @@
 class Aluno {
     constructor(nome, ra, idade, nota, sexo, media, resultado) {
-        this.nome = nome
-        this.ra = ra
-        this.idade = idade
-        this.nota = nota
-        this.sexo = sexo
-        this.media = media
-        this.resultado = resultado
+        this.nome = nome;
+        this.ra = ra;
+        this.idade = idade;
+        this.nota = nota;
+        this.sexo = sexo;
+        this.media = media;
+        this.resultado = resultado;
     }
 }
 
@@ -14,16 +14,23 @@ const alunos = []
 const output = document.getElementById("output")
 
 function cadastrarAluno() {
-    const nome = prompt("Digite o nome do aluno: ")
-    const ra = prompt("Digite o RA do aluno: ")
-    const idade = parseInt(prompt("Digite a idade do aluno: "))
-    const nota = parseFloat(prompt("Digite a nota do aluno: "))
-    const sexo = prompt("Digite o sexo do aluno: ")
-    const media = parseFloat(prompt("Digite a média do aluno: "))
-    const resultado = prompt("Digite o resultado do aluno: ")
+    const nome = document.getElementById("nome").value;
+    const ra = document.getElementById("ra").value;
+    const idade = parseInt(document.getElementById("idade").value);
+    const sexo = document.getElementById("sexo").value;
+    const media = parseFloat(document.getElementById("media").value);
+    const resultado = document.getElementById("resultado").value;
 
-    const aluno = new Aluno(nome, ra, idade, nota, sexo, media, resultado)
-    alunos.push(aluno)
+    const aluno = new Aluno(nome, ra, idade, sexo, media, resultado);
+    alunos.push(aluno);
+    renderAlunos(alunos);
+
+    // Limpa o formulário e o oculta
+    document.getElementById("alunoForm").reset();
+    document.getElementById("alunoForm").style.display = "none";
+    
+    // Resetar o menu de seleção
+    document.getElementById("selecao").value = "escolha";
 }
 
 function renderAlunos(alunosArray) {
@@ -34,33 +41,33 @@ function renderAlunos(alunosArray) {
         const row = document.createElement("tr")
 
         const nomeCell = document.createElement("td")
-        nomeCell.textContent = aluno.nome
-        row.appendChild(nomeCell)
+        nomeCell.textContent = aluno.nome;
+        row.appendChild(nomeCell);
 
         const raCell = document.createElement("td")
-        raCell.textContent = aluno.ra
-        row.appendChild(raCell)
+        raCell.textContent = aluno.ra;
+        row.appendChild(raCell);
 
         const idadeCell = document.createElement("td")
-        idadeCell.textContent = aluno.idade
-        row.appendChild(idadeCell)
+        idadeCell.textContent = aluno.idade;
+        row.appendChild(idadeCell);
 
         const notaCell = document.createElement("td")
-        notaCell.textContent = aluno.nota
-        row.appendChild(notaCell)
+        notaCell.textContent = aluno.nota;
+        row.appendChild(notaCell);
 
         const sexoCell = document.createElement("td")
-        sexoCell.textContent = aluno.sexo
-        row.appendChild(sexoCell)
+        sexoCell.textContent = aluno.sexo;
+        row.appendChild(sexoCell);
 
         const mediaCell = document.createElement("td")
-        mediaCell.textContent = aluno.media
-        row.appendChild(mediaCell)
+        mediaCell.textContent = aluno.media;
+        row.appendChild(mediaCell);
 
         const resultadoCell = document.createElement("td")
-        resultadoCell.textContent = aluno.resultado
-        row.appendChild(resultadoCell)
+        resultadoCell.textContent = aluno.resultado;
+        row.appendChild(resultadoCell);
 
-        tbody.appendChild(row)
+        tbody.appendChild(row);
     }
 }
