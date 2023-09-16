9204f872be04e5a001a79fa5f788f24d9157d72f
Leonardo Naves <112529255+Navesx0@users.noreply.github.com>
2023-09-16 18:33:58 -0300

    refactor: correção no cadastro do aluno.

--- Cadastrador.js
+++ Cadastrador.js
@@ -1,73 +1,83 @@
 class Aluno {
-    constructor(nome, ra, idade, nota, sexo, media, resultado) {
-        this.nome = nome;
-        this.ra = ra;
-        this.idade = idade;
-        this.nota = nota;
-        this.sexo = sexo;
-        this.media = media;
-        this.resultado = resultado;
-    }
+  constructor(nome, ra, idade, nota, sexo, media, resultado) {
+    this.nome = nome;
+    this.ra = ra;
+    this.idade = idade;
+    this.nota = nota;
+    this.sexo = sexo;
+    this.media = media;
+    this.resultado = resultado;
+  }
 }
 
-const alunos = []
-const output = document.getElementById("output")
+const alunos = [];
+const output = document.getElementById("output");
 
 function cadastrarAluno() {
-    const nome = document.getElementById("nome").value;
-    const ra = document.getElementById("ra").value;
-    const idade = parseInt(document.getElementById("idade").value);
-    const sexo = document.getElementById("sexo").value;
-    const media = parseFloat(document.getElementById("media").value);
-    const resultado = document.getElementById("resultado").value;
+  const nome = document.getElementById("nome").value;
+  const ra = document.getElementById("ra").value;
+  const idade = parseInt(document.getElementById("idade").value);
+  const sexo = document.getElementById("sexo").value;
+  const media = parseFloat(document.getElementById("media").value);
 
-    const aluno = new Aluno(nome, ra, idade, sexo, media, resultado);
-    alunos.push(aluno);
-    renderAlunos(alunos);
+  const resultadoAprovado = document.getElementById("aprovado");
+  const resultadoReprovado = document.getElementById("reprovado");
 
-    // Limpa o formulário e o oculta
-    document.getElementById("alunoForm").reset();
-    document.getElementById("alunoForm").style.display = "none";
-    
-    // Resetar o menu de seleção
-    document.getElementById("selecao").value = "escolha";
+  let resultadoValor = ""; // Inicialize como vazio
+
+  if (resultadoAprovado.checked) {
+    resultadoValor = resultadoAprovado.value;
+  } else if (resultadoReprovado.checked) {
+    resultadoValor = resultadoReprovado.value;
+  }
+
+  const aluno = new Aluno(nome, ra, idade, media, sexo);
+
+  // Use o setter 'resultado' para definir o valor
+  aluno.resultado = resultadoValor;
+
+  alunos.push(aluno);
+  console.log(alunos);
+
+  // Limpa o formulário e o oculta
+  document.getElementById("alunoForm").reset();
+  document.getElementById("alunoForm").style.display = "none";
+
+  // Resetar o menu de seleção
+  document.getElementById("selecao").value = "escolha";
 }
 
 function renderAlunos(alunosArray) {
-    const tbody = document.getElementById("output")
-    tbody.innerHTML = "" // Limpa a tabela antes de adicionar os novos dados
+  const tbody = document.getElementById("output");
+  tbody.innerHTML = ""; // Limpa a tabela antes de adicionar os novos dados
 
-    for (const aluno of alunosArray) {
-        const row = document.createElement("tr")
+  for (const aluno of alunosArray) {
+    const row = document.createElement("tr");
 
-        const nomeCell = document.createElement("td")
-        nomeCell.textContent = aluno.nome;
-        row.appendChild(nomeCell);
+    const nomeCell = document.createElement("td");
+    nomeCell.textContent = aluno.nome;
+    row.appendChild(nomeCell);
 
-        const raCell = document.createElement("td")
-        raCell.textContent = aluno.ra;
-        row.appendChild(raCell);
+    const raCell = document.createElement("td");
+    raCell.textContent = aluno.ra;
+    row.appendChild(raCell);
 
-        const idadeCell = document.createElement("td")
-        idadeCell.textContent = aluno.idade;
-        row.appendChild(idadeCell);
+    const idadeCell = document.createElement("td");
+    idadeCell.textContent = aluno.idade;
+    row.appendChild(idadeCell);
 
-        const notaCell = document.createElement("td")
-        notaCell.textContent = aluno.nota;
-        row.appendChild(notaCell);
+    const notaCell = document.createElement("td");
+    notaCell.textContent = aluno.nota;
+    row.appendChild(notaCell);
 
-        const sexoCell = document.createElement("td")
-        sexoCell.textContent = aluno.sexo;
-        row.appendChild(sexoCell);
+    const sexoCell = document.createElement("td");
+    sexoCell.textContent = aluno.sexo;
+    row.appendChild(sexoCell);
 
-        const mediaCell = document.createElement("td")
-        mediaCell.textContent = aluno.media;
-        row.appendChild(mediaCell);
+    const resultadoCell = document.createElement("td");
+    resultadoCell.textContent = aluno.resultado;
+    row.appendChild(resultadoCell);
 
-        const resultadoCell = document.createElement("td")
-        resultadoCell.textContent = aluno.resultado;
-        row.appendChild(resultadoCell);
-
-        tbody.appendChild(row);
-    }
+    tbody.appendChild(row);
+  }
 }
